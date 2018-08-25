a63cc547e89b0f5f0d7df5308fd106590abb83c2
otapi <otapi@protonmail.ch>
2018-08-25 23:06:53 +0200

    Add project files.

---
+++ FitMyFood/FitMyFood/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FitMyFood.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace FitMyFood
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
