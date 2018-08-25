19559a1269a6ca9a6c6305e5f744fbc6800dae63
otapi <otapi@protonmail.ch>
2018-08-26 01:26:03 +0200

    A bit more fw to MVVM

--- FitMyFood/FitMyFood/ViewModels/ItemDetailViewModel.cs
+++ FitMyFood/FitMyFood/ViewModels/ItemDetailViewModel.cs
@@ -7,7 +7,21 @@ namespace FitMyFood.ViewModels
     public class ItemDetailViewModel : BaseViewModel
     {
         public Item Item { get; set; }
+        public ItemDetailViewModel()
+        {
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+            init(item);
+        }
         public ItemDetailViewModel(Item item = null)
+        {
+            init(item);
+        }
+
+        void init(Item item = null)
         {
             Title = item?.Text;
             Item = item;
--- FitMyFood/FitMyFood/Views/ItemDetailPage.xaml.cs
+++ FitMyFood/FitMyFood/Views/ItemDetailPage.xaml.cs
@@ -11,27 +11,16 @@ namespace FitMyFood.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemDetailPage : ContentPage
     {
-        ItemDetailViewModel viewModel;
-
         public ItemDetailPage(ItemDetailViewModel viewModel)
         {
             InitializeComponent();
 
-            BindingContext = this.viewModel = viewModel;
+            BindingContext = viewModel;
         }
 
         public ItemDetailPage()
         {
             InitializeComponent();
-
-            var item = new Item
-            {
-                Text = "Item 1",
-                Description = "This is an item description."
-            };
-
-            viewModel = new ItemDetailViewModel(item);
-            BindingContext = viewModel;
         }
     }
 }
