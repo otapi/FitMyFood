68596c3ef4a4c93f03ad0397405d4ad14fae1e4f
otapi <otapi@protonmail.ch>
2018-10-17 18:31:36 +0200

    Rounded buttons, autoselect entries

---
+++ FitMyFood/FitMyFood.Android/Renderers/RoundButtonRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using FitMyFood.Droid.Renderers;
+using Android.Support.V7.Widget;
+
+[assembly:ExportRenderer (typeof(Xamarin.Forms.Button), typeof(RoundButtonRenderer))] 
+namespace FitMyFood.Droid.Renderers
+{
+    public class RoundButtonRenderer : ButtonRenderer
+    {
+        public RoundButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var button = (Xamarin.Forms.Button)e.NewElement;
+                button.CornerRadius = 22;
+            }
+        }
+
+    }
+}
---
+++ FitMyFood/FitMyFood.Android/Renderers/SelectAllOnFocusTextRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using FitMyFood.Droid.Renderers;
+using Android.Support.V7.Widget;
+using System.ComponentModel;
+
+[assembly:ExportRenderer (typeof(Xamarin.Forms.Entry), typeof(SelectAllOnFocusTextRenderer))] 
+namespace FitMyFood.Droid.Renderers
+{
+    public class SelectAllOnFocusTextRenderer : EntryRenderer
+    {
+        public SelectAllOnFocusTextRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(sender, args);
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    var nativeEditText = (global::Android.Widget.EditText)Control;
+                    nativeEditText.SetSelectAllOnFocus(true);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
