865b58474ad0c0c1259168a1b3eddb516c5a7059
otapi <otapi@protonmail.ch>
2018-10-30 00:19:32 +0100

    Kaloriabazis - works

--- FitMyFood/FitMyFood/RemoteParsers/KaloriaBazisRemoteParser.cs
+++ FitMyFood/FitMyFood/RemoteParsers/KaloriaBazisRemoteParser.cs
@@ -33,23 +33,95 @@ namespace FitMyFood.RemoteParsers
         public List<FoodItem> GetMatches(string pattern)
         {
             List<FoodItem> retnams = new List<FoodItem>();
-            // https://kaloriabazis.hu/getfood.php?fav=true&q=r%C3%A9pa&p=1&s=8&expropsearch_id=0&expropsearch_inc=0&all_public_food=0
-            dynamic client = new RestClient("https://kaloriabazis.hu");
-            
-            var result = client.Resource("getfood.php").Query(new {
-                fav = true,
-                q=pattern,
-                p=1,
-                s=8,
-                expropsearch_id = 0,
-                expropsearch_inc = 0,
-                all_public_food = 0
-                }).Get().Result;
-            
+            if (pattern != null && pattern.Length > 2)
+            {
+                // https://kaloriabazis.hu/getfood.php?fav=true&q=r%C3%A9pa&p=1&s=8&expropsearch_id=0&expropsearch_inc=0&all_public_food=0
+                dynamic client = new RestClient("https://kaloriabazis.hu");
+                KalObject result = null;
+                try
+                {
+                    result = client.Resource("getfood.php").Query(new
+                    {
+                        fav = true,
+                        q = pattern,
+                        p = 1,
+                        s = 8,
+                        expropsearch_id = 0,
+                        expropsearch_inc = 0,
+                        all_public_food = 0
+                    }).Get().Result;
+                } catch
+                {
+                    return retnams;
+                }
+                foreach (var f in result.results2)
+                {
+                    retnams.Add(new FoodItem()
+                    {
+                        Name = f.name.Replace("<b>", "").Replace("</b>",""),
+                        Protein = double.Parse(f.protein),
+                        Carbo = double.Parse(f.carbo),
+                        Fat = double.Parse(f.fat)
+
+                    });
+                }
+            }
+             
             return retnams;
         }
 
        
 
+    }
+
+ 
+
+    public class Results2
+    {
+        public string id { get; set; }
+        public string food_id { get; set; }
+        public string kuktalink { get; set; }
+        public string kuktapic { get; set; }
+        public string kuktatitle { get; set; }
+        public string votedpic { get; set; }
+        public string votedpictitle { get; set; }
+        public string name { get; set; }
+        public string cDesc { get; set; }
+        public string pic { get; set; }
+        public string pictitle { get; set; }
+        public string pictrash { get; set; }
+        public string pictrashtitle { get; set; }
+        public string piece { get; set; }
+        public string weight { get; set; }
+        public string cal { get; set; }
+        public string protein { get; set; }
+        public string carbo { get; set; }
+        public string fat { get; set; }
+        public string link { get; set; }
+        public string linkname { get; set; }
+        public string link2 { get; set; }
+        public string unvoteddelvotedpic { get; set; }
+        public string unvoteddelvotedpictitle { get; set; }
+        public string datasheetlink { get; set; }
+    }
+
+    public class UsdaLinksArr
+    {
+        public string __invalid_name__2315 { get; set; }
+        public string __invalid_name__2317 { get; set; }
+        public object __invalid_name__2335 { get; set; }
+        public string __invalid_name__432777 { get; set; }
+        public string __invalid_name__1502333 { get; set; }
+    }
+
+    public class KalObject
+    {
+        public int rownum1 { get; set; }
+        public string total1 { get; set; }
+        public List<object> results1 { get; set; }
+        public int total2 { get; set; }
+        public List<Results2> results2 { get; set; }
+        public UsdaLinksArr usdaLinks_arr { get; set; }
+        public List<object> rec_usda_empty_arr { get; set; }
     }
 }
--- FitMyFood/FitMyFood/ViewModels/VariationItemVM.cs
+++ FitMyFood/FitMyFood/ViewModels/VariationItemVM.cs
@@ -189,14 +189,13 @@ namespace FitMyFood.ViewModels
             {
                 SearchItems.Add(item);
             };
-            if (term != null && term.Length > 1)
+            
+            RemoteParsers.KaloriaBazisRemoteParser kaloriaBazisRemoteParser = new RemoteParsers.KaloriaBazisRemoteParser();
+            foreach (var item in kaloriaBazisRemoteParser.GetMatches(term))
             {
-                RemoteParsers.KaloriaBazisRemoteParser kaloriaBazisRemoteParser = new RemoteParsers.KaloriaBazisRemoteParser();
-                foreach (var item in kaloriaBazisRemoteParser.GetMatches(term))
-                {
-                    SearchItems.Add(item);
-                }
+                SearchItems.Add(item);
             }
+
         }
 
         private void SuggestWeight()
